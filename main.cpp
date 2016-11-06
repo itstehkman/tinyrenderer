326a22017d35a4adac573050acb0131e542aa9d5
itstehkman <itstehkman@gmail.com>
2016-11-05 18:11:48 -0700

    part 1 wireframe rendering done, onto part 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,51 @@
 #include "tgaimage.h"
+#include "geometry.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
 void line(int,int,int,int,TGAImage&, TGAColor);
+void try_line();  
+void try_faces(); 
 
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-
-    // Try out line func
-    line(13, 20, 80, 40, image, white);
-    line(10, 0, 11, 80, image, red);
-
-    image.flip_vertically(); // make origin left corner 
-    image.write_tga_file("line.tga");
+    
+    //try_line();
+    try_faces(); 
     return 0;
 }
 
+void try_line() {
+    TGAImage image(100, 100, TGAImage::RGB);
+    line(13, 20, 80, 40, image, white);
+    line(10, 0, 11, 80, image, red);
+    image.flip_vertically(); // make origin left corner 
+    image.write_tga_file("line.tga");
+}
 
+void try_faces() {
+    int height = 800, width = height;
+    TGAImage image(width, height, TGAImage::RGB);
+    Model *model = new Model("obj/african_head.obj");
+
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.)*width / 2.;
+            int y0 = (v0.y + 1.)*height / 2.;
+            int x1 = (v1.x + 1.)*width / 2.;
+            int y1 = (v1.y + 1.)*height / 2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+    
+    delete model;
+    image.flip_vertically();
+    image.write_tga_file("african_head.tga");
+}
 
 void line(int x0, int y0, int x1 ,int y1, TGAImage &image, TGAColor color) {
     
